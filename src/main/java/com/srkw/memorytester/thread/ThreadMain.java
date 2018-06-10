d7032a3240047fab5ca6065becd7095d9ce12f33
Unknown <kaptainwutax@gmail.com>
2018-06-09 20:00:32 -0400

    Null Fix

--- src/main/java/com/srkw/memorytester/thread/ThreadMain.java
+++ src/main/java/com/srkw/memorytester/thread/ThreadMain.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 
 import com.srkw.memorytester.gui.GuiMain;
 import com.srkw.memorytester.gui.GuiMenu;
