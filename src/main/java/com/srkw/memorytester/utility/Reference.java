154303e90c5019144bfef2139e640778b785ca1d
Unknown <kaptainwutax@gmail.com>
2018-06-09 20:45:29 -0400

    Version Fixed

--- src/main/java/com/srkw/memorytester/utility/Reference.java
+++ src/main/java/com/srkw/memorytester/utility/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
 
 	public static final String MOD_ID = "memorytester";
 	public static final String NAME = "MemoryTester";
-	public static final String VERSION = "Alpha";
+	public static final String VERSION = "0.1";
 	
 }
